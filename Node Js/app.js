2d32856d57820aab83f1af76b742fc699122dd60
Umair Sajawal <97627448+UmairSajawal@users.noreply.github.com>
2025-11-15 20:17:03 +0500

    Add files via upload

---
+++ Node Js/app.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+fs.writeFileSync('umair.txt', 'Hello I am Umair');
+
+const data = require('./data.js'); //importing data from data.js
+console.log(data);
+// console.log(10+10);
+
+// function fruit(item){
+//     console.log("I like" + item);
+// }
+// fruit(" mangoes");
+
+// var a = 2;
+// var b = 3;
+// console.log(a+b);
+
