eba5d72b34f6a6c9784ba6ed7d44b46400eb2f31
Umair Sajawal <97627448+UmairSajawal@users.noreply.github.com>
2025-11-18 20:44:59 +0500

    server.js
    Added usage notes and improved response message.

--- Node Js/server.js
+++ Node Js/server.js
@@ -1,7 +1,14 @@
 // Create server in Node Js
 
-const http = require('http');
+// Note: To run this file use command: node server.js
+// After running the command open browser and type: localhost:4800 
+// You will see the output in browser
+// to stop the server go to terminal and press: Ctrl + C
+
+const http = require("http");
 http.createServer((req, resp)=>{
     resp.write("<h1>Hi I am Umair Sajawal</h1>");
-    resp.end("Hello World"); // written .end() is important to end the response
+resp.end("Hello Umair"); // written .end() is important to end the response
 }).listen(4800);
+
+
